0f7798f9e076900be0624fc301758615e50097da
AT <nguyentronganhtuan2102@gmail.com>
2023-06-14 00:37:32 +0700

    All project

---
+++ RestaurantPOS/Migrations/20230613122019_tuan.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantPOS.Migrations
+{
+    public partial class tuan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
